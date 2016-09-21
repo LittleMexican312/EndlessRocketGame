31a9fe0cac43705a10a6742c03fc31e714e41e90
LittleMexican312 <Lukeflahavin@gmail.com>
2016-09-21 19:54:21 +1000

    Game States Sample

---
+++ state manager sample/cutscenestate.js
@@ -0,0 +1,21 @@
+
+var CutsceneState = function() 
+{
+	this.prototype = BaseState
+}
+
+CutsceneState.prototype.load = function() 
+{
+}
+
+CutsceneState.prototype.unload = function() 
+{
+}
+
+CutsceneState.prototype.update = function(dt) 
+{
+}
+
+CutsceneState.prototype.draw = function() 
+{
+}
