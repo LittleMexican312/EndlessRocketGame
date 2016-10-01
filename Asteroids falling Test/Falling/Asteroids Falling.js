f988e3fb410265e074080afd3a09c57fda508631
LittleMexican312 <Lukeflahavin@gmail.com>
2016-10-01 23:16:38 +1000

    update

--- Asteroids falling Test/Falling/Asteroids Falling.js
+++ Asteroids falling Test/Falling/Asteroids Falling.js
@@ -1,6 +1,5 @@
 var image="asteroid.png";
 var no = 15; // number of asteroids falling at once
-var time = 0; // Configure whether image should disappear after x seconds (0=never):
 var speed = 70; // falling speed
 var i, dwidth = 480, dheight =640; 
 var nht = dheight;
@@ -111,6 +110,3 @@ function animation() {  // animation function
 
 }
 animation();
-
-
-
